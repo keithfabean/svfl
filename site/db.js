97d625c22682b22ee0e75451936fbcd31f14fa9e
Keith Fabean <keithfabean@gmail.com>
2016-10-08 17:05:18 -0700

    fix the Heroku/Postgres db connection

--- site/db.js
+++ site/db.js
@@ -12,12 +12,23 @@ var env = process.env.NODE_ENV || 'development';
     if (env ==='production'){
         console.log("*** db.js 001 - PRODUCTION - ENTRY-POINT");
 
+        // Connection string for the Independant SVFL DataBase
+        // var pgClient = new pg.Client({
+        //     user: "ybiomfigpvqjzf",
+        //     password: "i-ClJgj2cxGEuFtasAWgQkTvBS",
+        //     database: "d4u42c1bctudfk",
+        //     port: 5432,
+        //     host: "ec2-54-163-238-215.compute-1.amazonaws.com",
+        //     ssl: true
+        // });
+
+        // Connection for th integrated SVFL database
         var pgClient = new pg.Client({
-            user: "ybiomfigpvqjzf",
-            password: "i-ClJgj2cxGEuFtasAWgQkTvBS",
-            database: "d4u42c1bctudfk",
+            user: "ljgifvgtcccpxh",
+            password: "sBsaLykzxWuXI7ZjR_QeZwpimM",
+            database: "d6t3kl9igl2kdc",
             port: 5432,
-            host: "ec2-54-163-238-215.compute-1.amazonaws.com",
+            host: "ec2-23-23-95-27.compute-1.amazonaws.com"
             ssl: true
         });
 
